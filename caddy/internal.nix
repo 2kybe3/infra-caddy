df5081c7bbe6cd7323de16adb990922207f0d986
2kybe3 <kybe@kybe.xyz>
2026-03-17 16:19:11 +0100

    fully migrate to nix

--- caddy/internal.nix
+++ caddy/internal.nix
@@ -1,5 +1,35 @@
-{
+let
+  MK-PROXY = ip: ''
+    encode
+    reverse_proxy ${ip}
+  '';
+in {
   services.caddy = {
-    extraConfig = builtins.readFile ./config/internal.txt;
+    virtualHosts = {
+      "frss.kybe.xyz".extraConfig = MK-PROXY " 10.0.4.18:8080";
+      "ppl.kybe.xyz".extraConfig = MK-PROXY "10.0.5.5:8000";
+      "translate.kybe.xyz".extraConfig = MK-PROXY "10.0.4.13:5000";
+      "mailadmin.kybe.xyz".extraConfig = MK-PROXY "10.0.4.3";
+      "search.kybe.xyz".extraConfig = MK-PROXY "10.0.5.7:8080";
+      "proxmox.kybe.xyz".extraConfig = MK-PROXY "http://10.0.5.1:8006";
+      "opn.kybe.xyz".extraConfig = MK-PROXY "10.0.4.1:8004";
+
+      # taken from public.nix
+      "git.kybe.xyz".extraConfig =
+        (MK-PROXY "10.0.4.12:3000")
+        + ''
+          handle_path / {
+            redir https://git.kybe.xyz/kybe 302
+          }
+        '';
+      "status.kybe.xyz".extraConfig = MK-PROXY "http://10.0.4.8:3001";
+      "gotify.kybe.xyz".extraConfig = MK-PROXY "10.0.4.19:8080";
+      "mastodon.kybe.xyz".extraConfig = MK-PROXY "10.0.4.20:80";
+      "matrix.kybe.xyz".extraConfig = MK-PROXY "10.0.4.6:6167";
+      "rhp.kybe.xyz".extraConfig = MK-PROXY "10.0.4.11:8080";
+      "reg.kybe.xyz".extraConfig = MK-PROXY "10.0.4.10:5000";
+      "uma.kybe.xyz".extraConfig = MK-PROXY "10.0.4.15:3000";
+      "i.kybe.xyz".extraConfig = MK-PROXY "10.0.4.16:80";
+    };
   };
 }
--- caddy/public.nix
+++ caddy/public.nix
@@ -1,13 +1,9 @@
 let
   CF-HEADER = "header_up X-Forwarded-For {http.request.header.CF-Connecting-IP}";
   CF-ONLY = "import cloudflare-only";
-  MK-PROXY = cf: ip: ''
+  MK-PROXY = ip: ''
     encode
-    ${
-      if cf
-      then CF-ONLY
-      else ""
-    }
+    ${CF-ONLY}
     reverse_proxy ${ip} {
       ${CF-HEADER}
     }
@@ -24,23 +20,23 @@ in {
       };
       "autodiscover.kybe.xyz" = {
         serverAliases = ["autoconfig.kybe.xyz"];
-        extraConfig = MK-PROXY true "10.0.4.3:80";
+        extraConfig = MK-PROXY "10.0.4.3:80";
       };
       "git.kybe.xyz".extraConfig =
-        (MK-PROXY true "10.0.4.12:3000")
+        (MK-PROXY "10.0.4.12:3000")
         + ''
           handle_path / {
             redir https://git.kybe.xyz/kybe 302
           }
         '';
-      "rhp.kybe.xyz".extraConfig = MK-PROXY true "10.0.4.11:8080";
-      "matrix.kybe.xyz".extraConfig = MK-PROXY true "10.0.4.6:6167";
-      "status.kybe.xyz".extraConfig = MK-PROXY true "http://10.0.4.8:3001";
-      "i.kybe.xyz".extraConfig = MK-PROXY true "10.0.4.16:80";
-      "gotify.kybe.xyz".extraConfig = MK-PROXY true "10.0.4.19:8080";
-      "mastodon.kybe.xyz".extraConfig = MK-PROXY true "10.0.4.20:80";
-      "reg.kybe.xyz".extraConfig = MK-PROXY true "10.0.4.10:5000";
-      "uma.kybe.xyz".extraConfig = MK-PROXY true "10.0.4.15:3000";
+      "status.kybe.xyz".extraConfig = MK-PROXY "http://10.0.4.8:3001";
+      "gotify.kybe.xyz".extraConfig = MK-PROXY "10.0.4.19:8080";
+      "mastodon.kybe.xyz".extraConfig = MK-PROXY "10.0.4.20:80";
+      "matrix.kybe.xyz".extraConfig = MK-PROXY "10.0.4.6:6167";
+      "rhp.kybe.xyz".extraConfig = MK-PROXY "10.0.4.11:8080";
+      "reg.kybe.xyz".extraConfig = MK-PROXY "10.0.4.10:5000";
+      "uma.kybe.xyz".extraConfig = MK-PROXY "10.0.4.15:3000";
+      "i.kybe.xyz".extraConfig = MK-PROXY "10.0.4.16:80";
     };
   };
 }
