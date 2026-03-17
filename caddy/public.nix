be5008d32ca2ce47f0c4abd776a3886204e0e472
2kybe3 <kybe@kybe.xyz>
2026-03-17 16:06:34 +0100

    migrate some endpoints to pure nix

--- caddy/public.nix
+++ caddy/public.nix
@@ -1,5 +1,22 @@
-{
+let
+  CF-HEADER = "header_up X-Forwarded-For {http.request.header.CF-Connecting-IP}";
+  CF-ONLY = "import cloudflare-only";
+  MK-PROXY = cf: ip: ''
+    encode
+    ${if cf then CF-ONLY else ""}
+    reverse_proxy ${ip} {
+      ${CF-HEADER}
+    }
+  '';
+in {
   services.caddy = {
     extraConfig = builtins.readFile ./config/public.txt;
+    virtualHosts = {
+      "kybe.xyz" = {
+        serverAliases = [ "www.kybe.xyz" ];
+        extraConfig = builtins.readFile ./config/kybe.xyz;
+      };
+      "rhp.kybe.xyz".extraConfig = MK-PROXY true "10.0.4.11:8080";
+    };
   };
 }
