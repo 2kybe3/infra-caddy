651309ed262f2d163d64a0a4d6f95bd64ee6f591
2kybe3 <kybe@kybe.xyz>
2026-03-17 16:28:07 +0100

    fix

--- caddy/internal.nix
+++ caddy/internal.nix
@@ -15,12 +15,12 @@ in {
   services.caddy = {
     virtualHosts = {
       "proxmox.kybe.xyz".extraConfig = MK-PROXY "10.0.5.1:8006" HTTPS-INSECURE;
-      "translate.kybe.xyz".extraConfig = MK-PROXY "10.0.4.13:5000";
-      "search.kybe.xyz".extraConfig = MK-PROXY "10.0.5.7:8080";
-      "frss.kybe.xyz".extraConfig = MK-PROXY "10.0.4.18:8080";
-      "mailadmin.kybe.xyz".extraConfig = MK-PROXY "10.0.4.3";
-      "ppl.kybe.xyz".extraConfig = MK-PROXY "10.0.5.5:8000";
-      "opn.kybe.xyz".extraConfig = MK-PROXY "10.0.4.1:8004";
+      "translate.kybe.xyz".extraConfig = MK-PROXY "10.0.4.13:5000" "";
+      "search.kybe.xyz".extraConfig = MK-PROXY "10.0.5.7:8080" "";
+      "frss.kybe.xyz".extraConfig = MK-PROXY "10.0.4.18:8080" "";
+      "mailadmin.kybe.xyz".extraConfig = MK-PROXY "10.0.4.3" "";
+      "ppl.kybe.xyz".extraConfig = MK-PROXY "10.0.5.5:8000" "";
+      "opn.kybe.xyz".extraConfig = MK-PROXY "10.0.4.1:8004" "";
 
       # taken from public.nix
       "git.kybe.xyz".extraConfig =
@@ -30,14 +30,14 @@ in {
             redir https://git.kybe.xyz/kybe 302
           }
         '';
-      "status.kybe.xyz".extraConfig = MK-PROXY "http://10.0.4.8:3001";
-      "gotify.kybe.xyz".extraConfig = MK-PROXY "10.0.4.19:8080";
-      "mastodon.kybe.xyz".extraConfig = MK-PROXY "10.0.4.20:80";
-      "matrix.kybe.xyz".extraConfig = MK-PROXY "10.0.4.6:6167";
-      "rhp.kybe.xyz".extraConfig = MK-PROXY "10.0.4.11:8080";
-      "reg.kybe.xyz".extraConfig = MK-PROXY "10.0.4.10:5000";
-      "uma.kybe.xyz".extraConfig = MK-PROXY "10.0.4.15:3000";
-      "i.kybe.xyz".extraConfig = MK-PROXY "10.0.4.16:80";
+      "status.kybe.xyz".extraConfig = MK-PROXY "http://10.0.4.8:3001" "";
+      "gotify.kybe.xyz".extraConfig = MK-PROXY "10.0.4.19:8080" "";
+      "mastodon.kybe.xyz".extraConfig = MK-PROXY "10.0.4.20:80" "";
+      "matrix.kybe.xyz".extraConfig = MK-PROXY "10.0.4.6:6167" "";
+      "rhp.kybe.xyz".extraConfig = MK-PROXY "10.0.4.11:8080" "";
+      "reg.kybe.xyz".extraConfig = MK-PROXY "10.0.4.10:5000" "";
+      "uma.kybe.xyz".extraConfig = MK-PROXY "10.0.4.15:3000" "";
+      "i.kybe.xyz".extraConfig = MK-PROXY "10.0.4.16:80" "";
     };
   };
 }
